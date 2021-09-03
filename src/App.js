541730b7168ffa6f7b344057147d49f72af8acb8
Clancinio <dean.clancy91@gmail.com>
2021-09-03 16:56:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Quote Generator</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
