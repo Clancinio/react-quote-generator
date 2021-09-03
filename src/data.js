282e6061c521d3dbe565117273d14e38f6661951
Clancinio <dean.clancy91@gmail.com>
2021-09-03 17:34:45 +0100

    Add dummy quotes

--- src/data.js
+++ src/data.js
@@ -1,40 +1,19 @@
-const reviews = [
+const quotes = [
   {
     id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum nunc nisl, imperdiet pharetra massa vestibulum quis. Ut nisl arcu, pretium in lorem non, blandit dictum elit.",
+    author: "Jerry Jefferson",
   },
   {
     id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-    text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    text: "Aenean condimentum bibendum elit, vel pharetra enim luctus ac. Donec rhoncus mollis tortor, vitae tincidunt quam fringilla in. Nullam vel tellus dictum, suscipit augue ac, imperdiet elit.",
+    author: "Jerry Jefferson",
   },
   {
     id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
-  },
-  {
-    id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
-    image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-    text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    text: "In erat dui, egestas vel aliquet sit amet, placerat vestibulum turpis. Praesent finibus magna arcu, vel congue lorem cursus nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque urna metus, aliquam eu laoreet in, faucibus quis est. Cras mollis vitae neque sed fermentum. ",
+    author: "Jerry Jefferson",
   },
 ];
 
-export default reviews;
+export default quotes;
